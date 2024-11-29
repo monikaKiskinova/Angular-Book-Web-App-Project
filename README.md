222e1f518d94ccc003c2d6bfe6f76e205af7ead3
Monika Kiskinova <monikova@abv.bg>
2024-11-29 07:19:22 +0200

    Add login form

---
+++ README.md
@@ -0,0 +1,11 @@
+... this project is a work in progress...
+
+
+
+Implementation: 
+1. Initialize project;
+2. Add header, footer and home view;
+3. Add 404 (Page Not Found) view;
+   with routing to the corresponging views when clicking on a link/button
+
+4. Add login form;
--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './main/home/home.component';
 import { PageNotFoundComponent } from './main/page-not-found/page-not-found.component';
+import { LoginComponent } from './main/user/login/login.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
     {path: '404', component: PageNotFoundComponent},
     {path: '**', redirectTo: '/404', pathMatch: 'full'},
 ];
